08a8d0753301e7b91494f2d5dead1ffe8542fc7f
muhammedriyas003 <muhammedriyas003@gmail.com>
2019-03-26 15:23:34 +0530

    js started

---
+++ HTML_CSS/js/add.js
@@ -0,0 +1,4 @@
+var x=123;
+var y=321;
+var z=x+y;
+console.log(z);
---
+++ HTML_CSS/js/grtnum.js
@@ -0,0 +1,20 @@
+var a=12;
+var b=15;
+var c=16;
+if (a>b && b>c)
+{
+
+console.log(a);
+}
+else if (b>c && c>a )
+
+{
+
+console.log(b);
+}
+
+else
+{
+console.log(c);
+
+}
