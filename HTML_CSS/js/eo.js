e8d898567f43d9ad5b874cad710b6b8924e79477
muhammedriyas003 <muhammedriyas003@gmail.com>
2019-03-26 16:17:03 +0530

    js continue

---
+++ HTML_CSS/js/eo.js
@@ -0,0 +1,14 @@
+var x=15;
+
+if( x%2 == 0 )
+{
+
+    console.log("even number");
+
+}
+else
+
+{
+
+    console.log("odd number");
+}
---
+++ HTML_CSS/js/while.js
@@ -0,0 +1,9 @@
+var i=1;
+
+while(i<=10)
+
+{
+
+console.log(i);
+i+=1;
+}
